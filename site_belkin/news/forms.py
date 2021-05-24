300d4b143a0014160471bf5795df64599cdd8237
CAPJACK <en.post.way@gmail.com>
2021-05-24 17:32:33 +0300

    WorkVersion

--- site_belkin/news/forms.py
+++ site_belkin/news/forms.py
@@ -2,6 +2,25 @@ from django import forms
 from .models import News
 import re  # Модуль регулярных выражений
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(label='e-mail', widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Подтверждение пароля',
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User  # Связываем с моделью User из Django
+        fields = ('username', 'email', 'password1', 'password2')
 
 
 class NewsForm(forms.ModelForm):
@@ -15,7 +34,7 @@ class NewsForm(forms.ModelForm):
             'category': forms.Select(attrs={'class': 'form-select'}),
         }
 
-    def clean_title(self): # Кастомный валидатор
+    def clean_title(self):  # Кастомный валидатор
         title = self.cleaned_data['title']
         if re.match(r'\d', title):
             raise ValidationError('Название не должно начинатся с цифры')
--- site_belkin/news/views.py
+++ site_belkin/news/views.py
@@ -1,16 +1,50 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView
-from .models import News, Category
-from .forms import NewsForm
+from .models import News
+from .forms import NewsForm, UserRegisterForm, UserLoginForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.contrib.auth import login, logout
 
 
-class HomeNews(LoginRequiredMixin, ListView):
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Вы успешно зарегистрированы')
+            return redirect('home')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'news/register.html', {"form": form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserLoginForm()
+    return render(request, 'news/login.html', {"form": form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+
+class HomeNews(ListView):
     login_url = '/admin/'
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
-    paginate_by = 2
+    paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -27,6 +61,7 @@ class NewsByCategory(LoginRequiredMixin, ListView):
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     allow_empty = False  # Заперщаем показ пустых списков (страниц)
+    paginate_by = 5
 
     # queryset = News.objects.select_related('category') - оптимизатор SQL запроса
     #
