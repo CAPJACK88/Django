d993dcb3b602825dbb4900d45c428652c7885a24
CAPJACK <en.post.way@gmail.com>
2021-05-25 18:20:46 +0300

    WorkVersion

--- manager/doc/models.py
+++ manager/doc/models.py
@@ -11,24 +11,24 @@ class Document(models.Model):
     category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
     username = models.ForeignKey('User', on_delete=models.PROTECT, verbose_name='Имя')
 
-    def __str__(self):
-        return self.title, self.description
-
-    class Meta:
-        verbose_name = 'Документ'
-        verbose_name_plural = 'Документы'
+    # def __str__(self):
+    #     return self.title
+    #
+    # class Meta:
+    #     verbose_name = 'Документ'
+    #     verbose_name_plural = 'Документы'
 
 
 class Category(models.Model):
     title = models.CharField(max_length=200, unique=True, verbose_name='Название')
     description = models.TextField(max_length=500, blank=True, verbose_name='Описание')
 
-    def __str__(self):
-        return self.title, self.description
-
-    class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
+    # def __str__(self):
+    #     return self.title
+    #
+    # class Meta:
+    #     verbose_name = 'Категория'
+    #     verbose_name_plural = 'Категории'
 
 
 class User(models.Model):
@@ -36,9 +36,9 @@ class User(models.Model):
     description = models.TextField(max_length=500, blank=True, verbose_name='Описание')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Фото')
 
-    def __str__(self):
-        return self.username, self.description
-
-    class Meta:
-        verbose_name = 'Имя'
-        verbose_name_plural = 'Имена'
+    # def __str__(self):
+    #     return self.username
+    #
+    # class Meta:
+    #     verbose_name = 'Имя'
+    #     verbose_name_plural = 'Имена'
