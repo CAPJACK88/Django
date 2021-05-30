8245cd46d9d20ca4581bc015f62badd885216bbd
CAPJACK88 <en.post.way@gmail.com>
2021-05-30 22:00:57 +0300

    home

---
+++ manager/cont/admin.py
@@ -0,0 +1,62 @@
+from django.contrib import admin
+from django import forms
+from .models import Contact, Company
+from django.utils.safestring import mark_safe
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class ContactAdminForm(forms.ModelForm):
+    comment = forms.CharField(label='Комментарий', widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Contact
+        fields = '__all__'
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'surname', 'email', 'telephone', 'mobile_phone', 'date_update', 'publications', 'get_photo',)
+    list_display_links = ('id', 'name', 'surname',)
+    search_fields = ('name', 'surname',)
+    list_filter = ('company', 'position')
+    list_editable = ('publications',)
+    fields = ('id', 'name', 'surname', 'middle_name', 'email', 'telephone', 'mobile_phone', 'url', 'date_birth',
+              'photo', 'get_photo', 'date_creation', 'date_update', 'comment', 'publications',)
+    readonly_fields = ('id', 'date_creation', 'date_update', 'get_photo',)
+    filter_horizontal = ('company', 'position',)
+    save_on_top = True
+    form = ContactAdminForm
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="75">')
+        else:
+            return 'Нет фото'
+
+    get_photo.short_description = 'Миниатюра'
+
+
+class CompanyAdminForm(forms.ModelForm):
+    comment = forms.CharField(label='Комментарий', widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Company
+        fields = '__all__'
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'inn', 'kpp', 'telephone', 'publications',)
+    list_display_links = ('id', 'name', 'inn',)
+    search_fields = ('name', 'inn', 'kpp', 'address', 'telephone', 'fixed',)
+    list_filter = ('fixed',)
+    list_editable = ('publications',)
+    fields = ('id', 'name', 'inn', 'kpp', 'address', 'telephone', 'url', 'photo', 'card', 'fixed', 'date_creation',
+              'date_update', 'comment', 'publications',)
+    readonly_fields = ('id', 'date_creation', 'date_update',)
+    filter_horizontal = ('fixed',)
+    save_on_top = True
+    form = CompanyAdminForm
+
+
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(Company, CompanyAdmin)
---
+++ manager/cont/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from user.models import User, Position
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=500, verbose_name='Имя')
+    surname = models.CharField(max_length=500, verbose_name='Фамилия')
+    middle_name = models.CharField(max_length=500, blank=True, verbose_name='Отчество')
+    email = models.EmailField(max_length=500, unique=True, verbose_name='Почта')
+    telephone = PhoneNumberField(blank=True, unique=True, verbose_name='Телефон')
+    mobile_phone = PhoneNumberField(blank=True, unique=True, verbose_name='Мобильный телефон')
+    url = models.URLField(max_length=200, blank=True, verbose_name='Веб-сайта')
+    date_birth = models.DateField(blank=True, verbose_name='Дата рождения')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Фото')
+    company = models.ManyToManyField('Company', blank=True, verbose_name='Компания')
+    position = models.ManyToManyField('user.Position', blank=True, verbose_name='Должность')
+    date_creation = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    date_update = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    comment = models.TextField(max_length=500, blank=True, verbose_name='Комментарий')
+    publications = models.BooleanField(default=False, verbose_name='Опубликовоно')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'
+        ordering = ['date_creation']
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=500, verbose_name='Название', help_text='ООО "НПО "АГАТ"')
+    inn = models.CharField(max_length=50, verbose_name='ИНН')
+    kpp = models.CharField(max_length=50, unique=True, verbose_name='КПП')
+    address = models.TextField(max_length=500, blank=True, verbose_name='Адрес',
+                               help_text='196247, Российская Федерация, город Санкт-Петербург, площадь Конституции, '
+                                         'дом 2')
+    telephone = PhoneNumberField(blank=True, verbose_name='Телефон')
+    photo = models.ImageField(upload_to='logo/%Y/%m/%d/', blank=True, verbose_name='Логотип')
+    card = models.FileField(upload_to='card/%Y/%m/%d/', blank=True, verbose_name='Карточка')
+    url = models.URLField(max_length=200, blank=True, verbose_name='Веб-сайта')
+    fixed = models.ManyToManyField('user.User', blank=True, verbose_name='Закреплено')
+    date_creation = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    date_update = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    comment = models.TextField(max_length=500, blank=True, verbose_name='Комментарий')
+    publications = models.BooleanField(default=False, verbose_name='Опубликовоно')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Компания'
+        verbose_name_plural = 'Компании'
+        ordering = ['date_creation']
