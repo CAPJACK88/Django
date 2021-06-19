a41fcee08d83c8e1a57e5ef8d2fd610de9e78c3a
CAPJACK88 <80195310+CAPJACK88@users.noreply.github.com>
2021-06-19 21:35:26 +0300

    Delete manager directory

--- manager/doc/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-
-
-class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(label=False, widget=forms.TextInput(
-        attrs={'class': 'form-control', 'type': 'text', 'placeholder': 'Имя пользователя'}))
-    password = forms.CharField(label=False, widget=forms.PasswordInput(
-        attrs={'class': 'form-control', 'type': 'password', 'placeholder': 'Пароль'}))
--- manager/score/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ScoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'score'
-    verbose_name = 'Книга Счетов'
